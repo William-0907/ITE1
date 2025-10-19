e6dc86ed9f27575d1c2e266fb556c9c07b8fb3e5
William-0907 <williamsotnac@gmail.com>
2025-10-19 22:34:16 +0800

    database connected to django

--- ITE1-Final_Project/Django/ITE1FinalDjango/ITE1FinalDjango/settings.py
+++ ITE1-Final_Project/Django/ITE1FinalDjango/ITE1FinalDjango/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_main',
 ]
 
 MIDDLEWARE = [
@@ -71,13 +72,18 @@ WSGI_APPLICATION = 'ITE1FinalDjango.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Sample',
+        'USER': 'root',  # or 'django_user' if you created one
+        'PASSWORD': '',  # your password if any
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
+
+
 
 
 # Password validation
