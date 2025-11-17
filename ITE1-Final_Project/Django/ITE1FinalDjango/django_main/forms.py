2616c1d1173deefc2ce67db02e215e38323e3017
William-0907 <williamsotnac@gmail.com>
2025-11-17 08:09:05 +0800

    new update

---
+++ ITE1-Final_Project/Django/ITE1FinalDjango/django_main/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Email',
+        'autofocus': True
+    }))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Username'
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Password'
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Confirm Password'
+    }))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("This username is already taken.")
+        return username
--- ITE1-Final_Project/Django/ITE1FinalDjango/django_main/views.py
+++ ITE1-Final_Project/Django/ITE1FinalDjango/django_main/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'django_main/index.html', {'title' : 'Home', 'name' : 'index'})
 
-def login(request):
-    return render(request, 'django_main/login.html', {'title' : 'Login', 'name' : 'login'})
+def loginRegister(request):
+    return render(request, 'django_main/login_register.html', {'title' : 'Login', 'name' : 'login'})
 
 def dashboard(request):
     return render(request, 'django_main/dashboard.html', {'title' : 'Dashboard', 'name' : 'userDashboard'})
